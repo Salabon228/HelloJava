185ad7d1c37619a7fccace6b6987be65dfde2196
Sergey <sergeistarshark@mail.ru>
2022-06-17 07:36:15 +0300

    add string.txt

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,10 @@
 
 public class Main {
     public static void main(String[] args) {
-        var a = 123;
-        System.out.println(a);  // 123
-        var d = 123.456;
-        System.out.println(d);  // 123.456
-        System.out.println(getType(a));  // Integer
-        System.out.println(getType(d));  // Double
-        d = 1022;
-        System.out.println(d);  // 1022
-        //d = "mistake";
-        //error: incompatible types:
-        //String cannot be converted to double
+        String s = "stroka";
+        System.out.println(s.charAt(2));
+        int d = s.length();
     }
-    static String getType(Object o){
-        return o.getClass().getSimpleName();
-    }
+
 
 }
