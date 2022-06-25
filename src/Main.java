90a9b9e98165428b07890246461d9f3ba5c1744e
Sergey <sergeistarshark@mail.ru>
2022-06-25 07:55:38 +0300

    update: add reader and writer

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
+import java.util.logging.*;
 public class Main {
     public static void main(String[] args) throws Exception {
 
-        String text = Files.ReadFile("input.txt"); // считывает текст из файла
+        Logger logger = Logger.getLogger(Main.class.getName()); // создаем логгиер и ук. имя класса в скобках
 
-        Files.WriteFile(text); // записывает считанный текст в файл(название файла пока не изменяется)
+        FileHandler fh = new FileHandler("log.txt");
+        logger.addHandler(fh);
+
+        SimpleFormatter sFormat = new SimpleFormatter();
+        fh.setFormatter(sFormat);
+        logger.log(Level.WARNING, "Тестовое логирование");
+        logger.info("Тестовое логирование2");
     }
 }
