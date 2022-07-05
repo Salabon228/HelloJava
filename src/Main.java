d2b6c51b872c4b00123c8e663d5f84782a24ad81
Sergey <sergeistarshark@mail.ru>
2022-07-05 07:10:35 +0300

    add lection 4

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,15 @@
-import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        // Logger.Hate();   в отдельном классе описан Логгер
-        ArrayList<Integer> li =  new ArrayList<>();
-        for (int i = 0; i <= 10; i++) {
-           li.add(i);
-        }
-        li.add(3,777);
-        System.out.println(li.remove(3)); // 777
-        System.out.println(li); // [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    public static void main(String[] args) {
+        Scanner iScanner = new Scanner(System.in);
+        System.out.printf("int a: ");
+        int x = iScanner.nextInt();
+        System.out.printf("double a: ");
+        double y = iScanner.nextDouble();
+        System.out.printf("%d + %f = %f", x, y, x + y);
+        iScanner.close();
+
     }
 }
+
---
+++ src/Task4.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Task4 {
+    public static void Hanoy(){
+        Scanner iScanner = new Scanner(System.in);
+        int disks = iScanner.nextInt();
+        System.out.println(disks);
+    }
+}
